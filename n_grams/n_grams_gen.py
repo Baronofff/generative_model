9679a1621b703e6e7244942a44d681c549c1e403
Baronofff <Baronov.ev@gmail.com>
2025-06-21 09:20:06 +0300

    final work

--- n_grams/n_grams_gen.py
+++ n_grams/n_grams_gen.py
@@ -2,7 +2,7 @@
 Generation by NGrams starter
 """
 from main import (BeamSearchTextGenerator, GreedyTextGenerator,
-                  NGramLanguageModel, TextProcessor)
+                  NGramLanguageModel, TextProcessor, BackOffGenerator)
 
 
 def main() -> None:
@@ -25,21 +25,42 @@ def main() -> None:
     model_10 = NGramLanguageModel(encoded, 10)
 
     model_7 = NGramLanguageModel(encoded, 7)
-    print("Model 4 built successfully?", model_10.build() == 0)  # Должно быть True
-    print("Model 7 built successfully?", model_7.build() == 0)  # Должно быть True
 
-    print("Number of 4-grams:", len(model_10._n_gram_frequencies))
+    model_5 = NGramLanguageModel(encoded, 5)
+
+    print("Model 10 built successfully?", model_10.build() == 0)  # Должно быть True
+    print("Model 7 built successfully?", model_7.build() == 0)  # Должно быть True
+    print("Model 5 built successfully?", model_5.build() == 0)  # Должно быть True
+
+    print('\n')
+
+    print("Number of 10-grams:", len(model_10._n_gram_frequencies))
     print("Number of 7-grams:", len(model_7._n_gram_frequencies))
+    print("Number of 5-grams:", len(model_5._n_gram_frequencies))
+
+    print('\n')
 
     print("greedy_text_generator:")
     greedy_text_generator = GreedyTextGenerator(model_10, processor)
     print(greedy_text_generator.run(50, 'Он понимал, что'))
     print(greedy_text_generator.run(50, 'с большою радостью готов был отправить'))
 
+    print('\n')
+
     print("beam_search_generator:")
     beam_search_generator = BeamSearchTextGenerator(model_7, processor, 10)
     print(beam_search_generator.run('Он понимал, что', 50))
     print(beam_search_generator.run('Он корточки приподня с', 50))
+
+    print('\n')
+
+    print("back_off_generator:")
+    back_off_generator = BackOffGenerator(
+        language_models=(model_10,
+                         model_7,
+                         model_5), text_processor=processor)
+    print(back_off_generator.run(prompt='Он понимал, что', seq_len=50))
+    print(back_off_generator.run(prompt='Он корточки приподня с', seq_len=50))
 
     assert result
 
